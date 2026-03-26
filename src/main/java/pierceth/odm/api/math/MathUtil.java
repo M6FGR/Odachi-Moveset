cb4ff076c5ff13f6b94ad6523140818bac780bc2
M6FGR <azooz77899@gmail.com>
2026-03-26 10:25:07 +0300

    Yes.

---
+++ src/main/java/pierceth/odm/api/math/MathUtil.java
@@ -0,0 +1,16 @@
+package pierceth.odm.api.math;
+
+import net.minecraft.world.phys.Vec3;
+
+public class MathUtil {
+    public static float limit(float val, float limit) {
+        return Math.min(val, limit);
+    }
+    public static int toHex(float r, float g, float b) {
+        int red = Math.round(r * 255.0F) << 16;
+        int green = Math.round(g * 255.0F) << 8;
+        int blue = Math.round(b * 255.0F);
+
+        return red | green | blue;
+    }
+}
