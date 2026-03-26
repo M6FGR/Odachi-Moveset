5f55c2f5a01ce8f32ba7abcc571564760b00d051
M6FGR <azooz77899@gmail.com>
2026-03-26 14:14:41 +0300

    Used the item's constructor and added new properties.

--- src/main/java/pierceth/odm/gameassets/OdachiItems.java
+++ src/main/java/pierceth/odm/gameassets/OdachiItems.java
@@ -2,17 +2,19 @@ package pierceth.odm.gameassets;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import pierceth.odm.OdachiMoveset;
 import pierceth.odm.api.cls.ILoadableClass;
 import pierceth.odm.api.registry.ItemsRegistry;
+import pierceth.odm.world.item.OdachiItem;
 
 public class OdachiItems implements ILoadableClass {
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, OdachiMoveset.MODID);
 
-    public static final DeferredHolder<Item, Item> ODACHI = ItemsRegistry.newItem("odachi", Item::new, ITEMS);
+    public static final DeferredHolder<Item, Item> ODACHI = ItemsRegistry.newItem("odachi", OdachiItem::new, ITEMS, properties -> properties.rarity(Rarity.RARE).durability(1980));
     @Override
     public void onModConstructor(IEventBus modBus) {
         ITEMS.register(modBus);
