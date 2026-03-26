66b6abe09f6832a55aacdb73953e3cad313fcf35
M6FGR <azooz77899@gmail.com>
2026-03-26 16:25:52 +0300

    A preset for testing the weapon

--- src/main/java/pierceth/odm/world/capabilities/item/WeaponCapabilityPreset.java
+++ src/main/java/pierceth/odm/world/capabilities/item/WeaponCapabilityPreset.java
@@ -13,34 +13,36 @@ import yesman.epicfight.gameasset.Animations;
 import yesman.epicfight.gameasset.ColliderPreset;
 import yesman.epicfight.registry.entries.EpicFightSkills;
 import yesman.epicfight.registry.entries.EpicFightSounds;
+import yesman.epicfight.world.capabilities.item.CapabilityItem.Styles;
 import yesman.epicfight.world.capabilities.item.CapabilityItem;
 import yesman.epicfight.world.capabilities.item.WeaponCapability;
 import yesman.epicfight.world.capabilities.item.WeaponCategory;
 
 import java.util.function.Function;
 
-import static yesman.epicfight.world.capabilities.item.CapabilityItem.Styles.MOUNT;
-import static yesman.epicfight.world.capabilities.item.CapabilityItem.Styles.TWO_HAND;
 
 public class WeaponCapabilityPreset implements ILoadableClass {
     public static final Function<Item, CapabilityItem.Builder<?>> ODACHI = (item) -> {
         return WeaponCapability.builder()
                 .category(OdachiCategories.ODACHI)
                 .styleProvider(playerPatch -> {
-                    return TWO_HAND;
+                    return Styles.TWO_HAND;
                 })
                 .hitSound(EpicFightSounds.BLADE_HIT.get())
                 .canBePlacedOffhand(false)
                 .swingSound(EpicFightSounds.WHOOSH_BIG.get())
                 .collider(ColliderPreset.TOOLS)
-                .newStyleCombo(TWO_HAND, OdachiAnimations.ODACHI_AUTO1, OdachiAnimations.ODACHI_AUTO2, OdachiAnimations.ODACHI_AUTO1, Animations.AXE_DASH, Animations.AXE_AIRSLASH)
-                .newStyleCombo(MOUNT, Animations.SWORD_MOUNT_ATTACK)
-                .innateSkill(TWO_HAND, (itemstack) -> {
+                .newStyleCombo(Styles.TWO_HAND,
+                        Animations.UCHIGATANA_AUTO1, Animations.UCHIGATANA_AUTO2, Animations.UCHIGATANA_AUTO3,
+                        Animations.AXE_DASH,
+                        Animations.AXE_AIRSLASH
+                )
+                .innateSkill(Styles.TWO_HAND, (itemstack) -> {
                     return EpicFightSkills.TSUNAMI.get();
                 })
-                .livingMotionModifier(TWO_HAND, LivingMotions.IDLE, OdachiAnimations.ODACHI_IDLE)
-                .livingMotionModifier(TWO_HAND, LivingMotions.WALK, OdachiAnimations.ODACHI_WALK)
-                .livingMotionModifier(TWO_HAND, LivingMotions.RUN, OdachiAnimations.ODACHI_RUN);
+                .livingMotionModifier(Styles.TWO_HAND, LivingMotions.IDLE, OdachiAnimations.ODACHI_IDLE)
+                .livingMotionModifier(Styles.TWO_HAND, LivingMotions.WALK, OdachiAnimations.ODACHI_WALK)
+                .livingMotionModifier(Styles.TWO_HAND, LivingMotions.RUN, OdachiAnimations.ODACHI_RUN);
     };
 
     private void registerCapability() {
