0c13b60c2481ce24987cd145f09d77652d677ccd
M6FGR <azooz77899@gmail.com>
2026-03-28 21:34:19 +0300

    Register new item: Rounded Shield

--- src/main/java/pierceth/odm/gameassets/OdachiItems.java
+++ src/main/java/pierceth/odm/gameassets/OdachiItems.java
@@ -3,6 +3,7 @@ package pierceth.odm.gameassets;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ShieldItem;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -14,7 +15,9 @@ import pierceth.odm.world.item.OdachiItem;
 public class OdachiItems implements ILoadableClass {
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, OdachiMoveset.MODID);
 
-    public static final DeferredHolder<Item, Item> ODACHI = ItemsRegistry.newItem("odachi", OdachiItem::new, ITEMS, properties -> properties.rarity(Rarity.RARE).durability(1980).attributes(OdachiItem.createOdachiAttributes()));
+    public static final DeferredHolder<Item, OdachiItem> ODACHI = ItemsRegistry.newItem("odachi", OdachiItem::new, ITEMS, properties -> properties.rarity(Rarity.RARE).durability(1980).attributes(OdachiItem.createOdachiAttributes()));
+
+    public static final DeferredHolder<Item, ShieldItem> ROUND_SHIELD = ItemsRegistry.newItem("round_shield", ShieldItem::new, ITEMS);
     @Override
     public void onModConstructor(IEventBus modBus) {
         ITEMS.register(modBus);
