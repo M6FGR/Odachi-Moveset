307503fc9e36eebc57b7f0b60b9270a466fcf507
M6FGR <azooz77899@gmail.com>
2026-03-28 21:36:24 +0300

    new Collider for the Odachi

---
+++ src/main/java/pierceth/odm/gameassets/OdachiColliders.java
@@ -0,0 +1,7 @@
+package pierceth.odm.gameassets;
+
+import yesman.epicfight.api.collider.MultiOBBCollider;
+
+public class OdachiColliders {
+    public static final MultiOBBCollider ODACHI = new MultiOBBCollider(4, 0.3, 0.3, 1.3, 0.0, 0.1, -1.2);
+}
